bfdb604e16ee3c5777cfca850ee0d6022c218cee
Rizik <rizikardi01@gmail.com>
2020-01-02 17:40:11 +0700

    Lanjutan belajar untuk membuat pilihan dan persetujuan peminjaman buku

--- Perpustakaan/src/app/Program.java
+++ Perpustakaan/src/app/Program.java
@@ -10,6 +10,7 @@ public class Program{
 	public static void main (String[] args){
 		//Buku buku1 = new Buku ("Sebelas Patriot", "2000-01-01", "Andrea Hirata", "Gramedia", 100)
 		HashMap <String, Buku> LemariBuku = new HashMap <String, Buku>();
+		HashMap <String, Peminjaman> DaftarTransaksi = new HashMap <String, Peminjaman>();
 		LemariBuku.put ("2000-01-01", new Buku ("Sebelas Patriot", "2000-01-01", "Andrea Hirata", "Gramedia", 100, 2011, 11));
 		LemariBuku.put ("2000-01-02", new Buku ("Duabelas Patriot", "2000-01-02", "Andrea Hirata", "Gramedia", 200, 2012, 12));
 		
@@ -24,6 +25,7 @@ public class Program{
 			int stok = LemariBuku.get(Key).getStok();
 			
 			//print variabel
+			System.out.println("DAFTAR BUKU");
 			System.out.println("No      : "+ No++);
 			System.out.println("Judul   : "+ judul);
 			System.out.println("ISBN    : "+ isbn);
@@ -47,6 +49,7 @@ public class Program{
 			String TglJoin = ListMember.get(Key).getTglJoin();
 			
 			//print variabel
+			System.out.println("DAFTAR MEMBER");
 			System.out.println("No       : "+ no++);
 			System.out.println("No. ID   : "+ id);
 			System.out.println("Nama     : "+ nama);
@@ -54,42 +57,81 @@ public class Program{
 			System.out.println();
 		}
 		Scanner input = new Scanner (System.in);
-		//input ISBN
-		System.out.print("Masukkan ISBN Buku yang Ingin Dipinjam : ");
-		String isbn = input.nextLine(); 
 		
-		Peminjaman transaction = new Peminjaman();
-		transaction setKodeTransaksi("T001");
-		//Cek buku dengan ISBN tersebut ada atau tidak
-		if (LemariBuku.containsKey(isbn)){
-			//Jika buku ditemukan, kurangi stok buku
-			System.out.println("Buku Ada");
-			Buku borrowedBook = LemariBuku.get(isbn);
-			borrowedBook.Dipinjam();
+		Member member = ListMember.get ("1933-01");
+		Peminjaman transaction = new Peminjaman(member);
+		transaction.setKodeTransaksi();
+		//System.out.println (transaction.getKodeTransaksi());
+		
+		boolean runPinjam = true;
+		while (runPinjam){
+			//input ISBN
+			System.out.print("Masukkan ISBN Buku yang Ingin Dipinjam : ");
+			String isbn = input.nextLine(); 
+			//Cek buku dengan ISBN tersebut ada atau tidak
+			if (LemariBuku.containsKey(isbn)){
+				//transaksi ketika suatu buku dipinjam
+				//Jika buku ditemukan, kurangi stok buku
+
+				Buku borrowedBook = LemariBuku.get(isbn);
+				borrowedBook.Dipinjam(); // method yang digunakan untuk MENGURANGI STOK BUKU
+				//Masukkan buku ke list buku yang ada di transaksi
+				transaction.addBook (isbn, borrowedBook);
+					int nomor = 1;
+				for (String Key : LemariBuku.keySet()){
 			
-				int nomor = 1;
-			for (String Key : LemariBuku.keySet()){
+					//Get data buku satu per satu masukkan ke variabel
+					String judul = LemariBuku.get(Key).getJudulBuku();
+					String ISBN = LemariBuku.get(Key).getISBN();
+					String penulis = LemariBuku.get(Key).getPenulis();
+					int stok = LemariBuku.get(Key).getStok();
 			
-				//Get data buku satu per satu masukkan ke variabel
-				String judul = LemariBuku.get(Key).getJudulBuku();
-				String ISBN = LemariBuku.get(Key).getISBN();
-				String penulis = LemariBuku.get(Key).getPenulis();
-				int stok = LemariBuku.get(Key).getStok();
-			
-				//print variabel
-				System.out.println("No      : "+ nomor++);
+					//print variabel
+					System.out.println("DAFTAR BUKU");
+					System.out.println("No      : "+ nomor++);
+					System.out.println("Judul   : "+ judul);
+					System.out.println("ISBN    : "+ isbn);
+					System.out.println("Penulis : "+ penulis);
+					System.out.println("Stock   : "+ stok);
+					System.out.println();
+				}	
+			}
+			else{
+				//jika tidak ditemukan, tampilkan pesan buku tidak ada
+				System.out.println("Buku Tidak Ada");
+			}
+			//transaksi buku dipinjam
+			System.out.print("Apakah Masih Ingin Meminjam Buku yang Lain? [Y/N] : ");
+			String answer = input.nextLine();
+			if (answer.equalsIgnoreCase("N")){
+				runPinjam = false;
+			}
+		}
+		if(transaction.getBooks().size() > 0){
+			for(String i : transaction.getBooks().keySet()){
+				String judul = LemariBuku.get(i).getJudulBuku();
+				String isbn = LemariBuku.get(i).getISBN();
+				String penulis = LemariBuku.get(i).getPenulis();
+				//Print Variable
+				System.out.println("DAFTAR BUKU");
+				System.out.println("No      : "+ No++);
 				System.out.println("Judul   : "+ judul);
 				System.out.println("ISBN    : "+ isbn);
 				System.out.println("Penulis : "+ penulis);
-				System.out.println("Stock   : "+ stok);
 				System.out.println();
 			}
-			
+			System.out.println("Apakah Data Di Atas Sudah Benar? Pilih Y untuk meminjam. [Y/N] : ");
+			String answer = input.nextLine();
+			if(answer.equalsIgnoreCase("Y")){
+				//masukkan data transaksi ke list transaksi (HashMap)
+				DaftarTransaksi.put(transaction.getKodeTransaksi(), transaction);
+			}
+			else{
+				System.out.println("Transaksi Peminjaman Dibatalkan.");
+			}
 		}
 		else{
-			//jika tidak ditemukan, tampilkan pesan buku tidak ada
-			System.out.println("Buku Tidak Ada");
+			System.out.println("Buku Apa yang Ingin Anda Pinjam? Anda Belum Meminjam Buku Apapun");
 		}
-		//transaksi buku dipinjam
 	}
 }
--- Perpustakaan/src/app/transaction/Peminjaman.java
+++ Perpustakaan/src/app/transaction/Peminjaman.java
@@ -2,7 +2,7 @@ package app.transaction;
 import app.data.Buku;
 import app.data.Member;
 
-import java.util.Hashmap;
+import java.util.HashMap;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,25 +15,27 @@ public class Peminjaman{
 	public Peminjaman (Member member){//butuh data member karena untuk kejelasan ini transaksi milik member siapa
 		this.member = member;
 		Date today = new Date();
-		this.dateTime = today
+		this.dateTime = today;
 	}
 	
 	public String getKodeTransaksi(){
 		return KodeTransaksi;
 	}
 	
-	public String setKodeTransaksi(){
+	public void setKodeTransaksi(){
 		this.KodeTransaksi = KodeTransaksi;
 		SimpleDateFormat formatter = new SimpleDateFormat ("dd-MM-yyyy hh:mm:ss");
+		String date = formatter.format (this.dateTime);
 		this.KodeTransaksi = this.member.getID() + "-" + date;
 	}
 	//get semua data buku dalam satu transaksi
-	public HashMap <String, Buku> getbooks(){
+	public HashMap <String, Buku> getBooks(){
 		return BukuDipinjam;
 	}
 	//tambahkan satu buku yang mau dipinjam (dijalankan ketika meminjam buku)
 	public void addBook (String isbn, Buku buku){
 		this.BukuDipinjam.put (isbn, buku);
+		System.out.println ("Buku " + BukuDipinjam.get(isbn).getJudulBuku() + " Berhasil Ditambahkan ke Transaksi"); //lebih baik, karena ditampilkan setelah buku benar benar dimasukkan ke peminjaman
 	}
 	//get satu buku yang dipinjam berdasarkan isbn
 	public Buku getBook (String isbn){
